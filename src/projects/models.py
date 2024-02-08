a755b7590348c9df764df4d36fc75c8aee297b42
dr-rompecabezas <felavid@gmail.com>
2024-02-07 20:46:11 -0500

    Update project title field and add slug generation in Project model

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 User = settings.AUTH_USER_MODEL
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(unique=True)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     active = models.BooleanField(default=True)
@@ -14,3 +15,8 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
