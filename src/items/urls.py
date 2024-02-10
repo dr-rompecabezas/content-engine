2024419173e2c0ef84788e2cc9d4c69597e4a7ea
dr-rompecabezas <felavid@gmail.com>
2024-02-10 15:26:37 -0500

    Enable item update and delete functionality

--- src/items/urls.py
+++ src/items/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.item_list_view, name="list"),
     path("<int:id>/", views.item_detail_view, name="detail"),
     path("create/", views.item_create_view, name="create"),
-    # path("<int:id>/update/", views.item_update_view, name="update"),
-    # path("<int:id>/delete/", views.item_delete_view, name="delete"),
+    path("<int:id>/update/", views.item_update_view, name="update"),
+    path("<int:id>/delete/", views.item_delete_view, name="delete"),
 ]
