5f21beabc6e83977fc7f8c5a5d36ef0feb572d14
dr-rompecabezas <felavid@gmail.com>
2024-02-09 19:21:36 -0500

    Add items app and Item model

--- src/config/settings.py
+++ src/config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "landing",
     "users",
     "projects",
+    "items",
 ]
 
 MIDDLEWARE = [
---
+++ src/items/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+from projects.models import Project
+
+User = settings.AUTH_USER_MODEL
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name="items_created", on_delete=models.SET_NULL, null=True)
+    modified = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(User, related_name="items_modified", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
