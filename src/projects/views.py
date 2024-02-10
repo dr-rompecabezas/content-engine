08b9b7bcda8d9b4899da0214c7ecd5aff607ad4f
dr-rompecabezas <felavid@gmail.com>
2024-02-09 19:09:06 -0500

    Refactor project views and add error handling

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+from .models import Project
+
+
+def delete_project_from_session(request):
+    if "project_slug" in request.session:
+        del request.session["project_slug"]
+    return redirect("/")
 
 
 def activate_project_view(request, slug=None):
-    print(slug)
+    try:
+        project = Project.objects.get(owner=request.user, slug=slug)
+    except Project.DoesNotExist:
+        project = None
+    if project is None:
+        messages.error(request, "Project could not be activated")
+        delete_project_from_session(request)
+    request.session["project_slug"] = slug
+    messages.success(request, "Project activated")
     return redirect("/")
 
 
 def deactivate_project_view(request, slug=None):
+    delete_project_from_session(request)
     return redirect("/")
