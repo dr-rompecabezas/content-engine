afeab917ce6c634891cc9ba09f238cdb6060af20
dr-rompecabezas <felavid@gmail.com>
2024-02-10 15:26:50 -0500

    Update views.py with item update and delete functionality

--- src/items/views.py
+++ src/items/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 
-from .forms import ItemCreateForm
+from .forms import ItemCreateForm, ItemUpdateForm
 from .models import Item
 
 
@@ -27,8 +27,36 @@ def item_create_view(request):
         item.project = request.project
         item.created_by = request.user
         item.save()
-        form = ItemCreateForm()
+        return redirect(item.get_absolute_url())
     context = {
         "form": form
     }
     return render(request, "items/create.html", context)
+
+
+@login_required
+def item_update_view(request, id=None):
+    item = get_object_or_404(Item, id=id, project=request.project)
+    form = ItemUpdateForm(request.POST or None, instance=item)
+    if form.is_valid():
+        item = form.save(commit=False)
+        item.last_modified_by = request.user
+        item.save()
+        return redirect(item.get_absolute_url())
+    context = {
+        "item": item,
+        "form": form,
+    }
+    return render(request, "items/update.html", context)
+
+
+@login_required
+def item_delete_view(request, id=None):
+    item = get_object_or_404(Item, id=id, project=request.project)
+    if request.method == "POST":
+        item.delete()
+        return redirect("items:list")
+    context = {
+        "item": item,
+    }
+    return render(request, "items/delete.html", context)
