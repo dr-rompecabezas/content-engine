464323ae7f0d7e141d922489671bb60743d78404
dr-rompecabezas <felavid@gmail.com>
2024-02-10 14:26:18 -0500

    Add list and detail item views

--- src/items/views.py
+++ src/items/views.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .forms import ItemCreateForm
 from .models import Item
+
+
+@login_required
+def item_list_view(request):
+    items = Item.objects.filter(project=request.project)
+    return render(request, "items/list.html", {"items": items})
+
+
+@login_required
+def item_detail_view(request, id=None):
+    item = get_object_or_404(Item, id=id, project=request.project)
+    return render(request, "items/detail.html", {"item": item})
 
 
 @login_required
@@ -19,4 +31,4 @@ def item_create_view(request):
     context = {
         "form": form
     }
-    return render(request, "items/item_create.html", context)
+    return render(request, "items/create.html", context)
