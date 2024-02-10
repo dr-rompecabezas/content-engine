2bbc7e088653061fa9da4183aa9cc3472f4f7686
dr-rompecabezas <felavid@gmail.com>
2024-02-10 13:15:21 -0500

    Add items app URLs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from landing import views as landing_views
 from projects import views as project_views
@@ -7,6 +7,7 @@ from projects import views as project_views
 
 urlpatterns = [
     path("", landing_views.home, name="home"),
+    path("items/", include("items.urls")),
     path("activate/project/<slug:slug>/", project_views.activate_project_view, name="activate_project"),
     path("deactivate/project/<slug:slug>/", project_views.deactivate_project_view, name="deactivate_project"),
     path("admin/", admin.site.urls),
---
+++ src/items/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "items"
+urlpatterns = [
+    path("create/", views.item_create_view, name="create"),
+    # path("<int:id>/", views.item_detail_view, name="detail"),
+    # path("<int:id>/update/", views.item_update_view, name="update"),
+    # path("<int:id>/delete/", views.item_delete_view, name="delete"),
+    # path("", views.item_list_view, name="list"),
+]
