7350257d359c4098bd3b049c17df3d45137134e3
dr-rompecabezas <felavid@gmail.com>
2024-02-10 20:05:26 -0500

    Refactor project URLs

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from landing import views as landing_views
-from projects import views as project_views
 
 
 urlpatterns = [
     path("", landing_views.home, name="home"),
     path("items/", include("items.urls")),
-    path("activate/project/<slug:slug>/", project_views.activate_project_view, name="activate_project"),
-    path("deactivate/project/<slug:slug>/", project_views.deactivate_project_view, name="deactivate_project"),
+    path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
 ]
