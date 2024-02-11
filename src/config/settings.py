7fe06438383cf21f22a43887762d502f5720f790
dr-rompecabezas <felavid@gmail.com>
2024-02-11 18:00:46 -0500

    Add django-htmx to INSTALLED_APPS and requirements.txt

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_htmx",
     "core",
     "users",
     "projects",
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
     "projects.middleware.ProjectMiddleware",
 ]
 
