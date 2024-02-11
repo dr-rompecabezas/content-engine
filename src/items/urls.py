e2e73ebf5f7c4a4cf2a6e07005740daed116ef2d
dr-rompecabezas <felavid@gmail.com>
2024-02-10 20:05:35 -0500

    Update item URLs

--- src/items/urls.py
+++ src/items/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "items"
 urlpatterns = [
     path("", views.item_list_view, name="list"),
-    path("<int:id>/", views.item_detail_view, name="detail"),
     path("create/", views.item_create_view, name="create"),
+    path("<int:id>/", views.item_detail_view, name="detail"),
     path("<int:id>/update/", views.item_update_view, name="update"),
     path("<int:id>/delete/", views.item_delete_view, name="delete"),
 ]
