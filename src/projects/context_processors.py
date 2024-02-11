634297b0fe33f64cd7c75fd8e16c71845e27f2c7
dr-rompecabezas <felavid@gmail.com>
2024-02-11 16:10:13 -0500

    Add user_projects context to context_processors.py

--- src/projects/context_processors.py
+++ src/projects/context_processors.py
@@ -1,9 +1,13 @@
 from . import cache as project_cache
 
+from .models import Project
+
 
 def user_projects_context(request):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
         user_projects = project_cache.get_user_projects(username=username)
+        print(user_projects)
         return {'user_projects': user_projects}
+    return {'user_projects': Project.objects.none()}
