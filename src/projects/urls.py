a0d0d829a752f835a35a61a774c6ac85ff81fc35
dr-rompecabezas <felavid@gmail.com>
2024-02-11 10:16:09 -0500

    Add activate and deactivate project views to project URLs

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -6,9 +6,9 @@ app_name = "projects"
 urlpatterns = [
     path("", views.project_list_view, name="list"),
     path("create/", views.project_create_view, name="create"),
+    path("deactivate/", views.deactivate_project_view, name="deactivate"),
     path("<slug:slug>/", views.project_detail_view, name="detail"),
+    path("<slug:slug>/activate/", views.activate_project_view, name="activate"),
     path("<slug:slug>/update/", views.project_update_view, name="update"),
     path("<slug:slug>/delete/", views.project_delete_view, name="delete"),
-    path("<slug:slug>/activate/", views.activate_project_view, name="activate_project"),
-    path("<slug:slug>/deactivate/", views.deactivate_project_view, name="deactivate_project"),
 ]
